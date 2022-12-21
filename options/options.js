6bc224a483225a12e6013e9ef1a3a447cac7971c
Crowdbotics <team@crowdbotics.com>
2022-12-21 21:08:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pendo_studio_long_l_37982",
+  url: "https://pendo_studio_long_l_37982.botics.co",
+  api: "https://pendo_studio_long_l_37982.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
